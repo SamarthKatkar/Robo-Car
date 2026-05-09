b6b89cc6f3fbf5682c884493a76b10514b8b048f
Samarth Katkar <samarthkatkar69@gmail.com>
2026-05-10 01:15:32 +0530

    Implement distance sensing and movement control
    Added distance sensing and obstacle avoidance logic to the Robo Car.

--- Robo_Car.ino
+++ Robo_Car.ino
@@ -1,4 +1,13 @@
+const int trigPin = A1;
+const int echoPin = A0; 
+int leftspeed=70;
+int rightspeed=90;
+int distance;
+
 void setup() {
+  Serial.begin(9600); 
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
  pinMode(3,OUTPUT);
  pinMode(5,OUTPUT);
  pinMode(9,OUTPUT);
@@ -7,15 +16,75 @@ void setup() {
 }
 
 void loop() {
-  int leftspeed=70;
-  int rightspeed=90;
+  dist();
+  forward();
+ 
+ if(distance<=20&&distance>0)
+ {
+   while(distance<30)
+   {
+     reverse();
+     dist();
+     delay(50);
+   }
+   stop();
+   delay(200);
+   turn();
+   delay(200);
+   stop();
+   delay(200);
+ }
+ else
+ {
+  forward();
+ }
+}
+
+void reverse()
+{
+  analogWrite(9,LOW);
+  analogWrite(10,leftspeed);
+  analogWrite(3,LOW);
+  analogWrite(5,rightspeed);
+}
+void forward()
+{
   analogWrite(9,leftspeed);
   analogWrite(10,LOW);
   analogWrite(3,rightspeed);
   analogWrite(5,LOW);
-  
+}
+void stop()
+{
+  analogWrite(9,LOW);
+  analogWrite(10,LOW);
+  analogWrite(3,LOW);
+  analogWrite(5,LOW);
+}
+void turn()
+{
+   analogWrite(9,LOW);
+  analogWrite(10,leftspeed);
+  analogWrite(3,rightspeed);
+  analogWrite(5,LOW);
+}
+void dist()
+{
+   // for clearing the pin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
 
- 
+  //to send a 10 microsecond wave
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
 
+  //to measure how lond echo pin stays high
+  long duration = pulseIn(echoPin, HIGH);
+
+ // Calculating Distance
+  distance = duration * 0.034 / 2;
 
 }
+
+
